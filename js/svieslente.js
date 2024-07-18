ea1ff4f294277ff767312db86eb2a08f68139ef5
Rasa <rasa.zukauskaite@gmail.com>
2024-07-18 11:15:25 +0300

    scoreboard

--- js/svieslente.js
+++ js/svieslente.js
@@ -12,6 +12,14 @@ const result2DOM = document.querySelector('.b_komanda > .final2');
 
 const summaryDOM = document.querySelector('section > div');
 
+const localData = localStorage.getItem('scores');
+let scoresData = [];
+
+if (localData !== null) {
+    scoreData = JSON.parse(localData);
+    renderList();
+}
+
 
 let total = 0;
 let sumA = 0;
@@ -19,19 +27,19 @@ let sumA = 0;
 function plusAOne() {
     total++;
     result1DOM.textContent = total;
-    sumA = `<p>&bull; A komanda +1</p>`;
+    sumA = `<p>${Date.now()}; A komanda + 1;</p> `;
     summaryDOM.innerHTML += sumA;
 }
 function plusATwo() {
     total += 2;
     result1DOM.textContent = total;
-    sumA = `<p>&bull; A komanda +2</p>`;
+    sumA = `<p>${new Date()}; A komanda + 2</p> `;
     summaryDOM.innerHTML += sumA;
 }
 function plusAThree() {
     total += 3;
     result1DOM.textContent = total;
-    sumA = `<p>&bull; A komanda +3</p>`;
+    sumA = `<p>A komanda + 3</p> `;
     summaryDOM.innerHTML += sumA;
 }
 plus1aDOM.addEventListener('click', plusAOne);
@@ -47,22 +55,92 @@ let sumB = 0;
 function plusBOne() {
     total2++;
     result2DOM.textContent = total2;
-    sumB = `<p>&bull; B komanda +1</p>`;
+    sumB = `<p>B komanda + 1</p> `;
     summaryDOM.innerHTML += sumB;
 }
 function plusBTwo() {
     total2 += 2;
     result2DOM.textContent = total2;
-    sumB = `<p>&bull; B komanda +2</p>`;
+    sumB = `<p>B komanda + 2</p> `;
     summaryDOM.innerHTML += sumB;
 }
 function plusBThree() {
     total2 += 3;
     result2DOM.textContent = total2;
-    sumB = `<p>&bull; B komanda +3</p>`;
+    sumB = `<p>B komanda + 3</p> `;
     summaryDOM.innerHTML += sumB;
 }
 plus1bDOM.addEventListener('click', plusBOne);
 plus2bDOM.addEventListener('click', plusBTwo);
 plus3bDOM.addEventListener('click', plusBThree);
+
+
+
+
+submitButtonDOM.addEventListener('click', e => {
+    e.prevent.Default();
+
+    scoresData.push({
+        createdAt: Date.now(),
+        text: summaryDOM.value,
+        state: 'scores',
+    });
+    localStorage.setItem('scores', JSON.stringify(scoresData));
+    renderList();
+});
+
+function renderList() {
+    if (scoresData.length === 0) {
+        renderEmptyList();
+    } else {
+        renderTaskList();
+    }
+}
+
+function renderTaskList() {
+    let HTML = '';
+
+    for (const scores of scoresData) {
+        HTML += `
+
+        <section data-state="${scores.state}">
+            <div>${formatTime(scores.createdAt)}</div>
+            <div>${scores.text}</div>
+            <div>
+                <button>Delete</button>
+            </div>
+        </section>`;
+    }
+
+    const sectionDOM = document.querySelector('section');
+
+    for (let i = 0; i < sectionDOM.length; i++) {
+        const sectionDOM = sectionDOM[i];
+
+        const deleteDOM = sectionDOM.querySelector('button.delete');
+        deleteDOM.addEventListener('click', () => {
+            scoresData.splice(i, 1);
+            renderList();
+        })
+    }
+}
+
+
+function formatTime(timeInMs) {
+
+    const date = new Date(timeInMs);
+    const y = date.getFullYear();
+    const m = (date.getMonth() < 9 ? '0' : '') + (date.getMonth() + 1);
+    const d = (date.getDate() < 10 ? '0' : '') + date.getDate();
+    const h = date.getHours();
+    const mn = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+    const s = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+
+    return `${y} -${m} -${d} ${h}:${mn}:${s} `;
+
+}
+
+
+
+
 
